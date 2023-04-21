e5de9c72e50ac2ed2903d4669e00b637e24c9cf7
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-21 06:40:49 -0700

    updated still WIP

--- pre-shell/eeaao.c
+++ pre-shell/eeaao.c
@@ -1,8 +1,55 @@
-#include "main.c"
+#include "main.h"
+
+char *_strdup(char *str)
+{
+	unsigned int i, length;
+	char *arr;
+
+	if (!str)
+		return (NULL);
+
+	for (length = 0; str[length]; length++)
+		;
+
+	arr = malloc(sizeof(char) * length + 1);
+
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		arr[i] = str[i];
+
+	return (arr);
+}
 
 char *string_parse(char *restrict str)
 {
-	
+	char **array; /* array to return */	
+	char *str_cp = malloc(sizeof(str));
+	char *token;
+	size_t count = 0;
+	size_t i;
+
+	str_cp = _strdup(str);
+
+	token = strtok(str_cp, " ");
+	while (token != NULL)
+	{
+		token = strtok(NULL, " ");
+		count++;
+	}
+	/* free copy of string */
+	free(str_cp);
+
+	array = malloc(sizeof(char *) * count);
+	token = strtok(str, " ");
+	for (i = 0; i < count; i++)
+	{
+		array[i] = _strdup(token);
+		printf("%s\n", array[i]);
+		token = strtok(NULL, " ");
+	}
+	return (*array);
 }
 
 
@@ -10,13 +57,14 @@ int main(void)
 {
 	char *input = malloc(1); /* recieved from terminal */
 	size_t len = 1; /* length */
-	char *arg = NULL; /* recieved argument array */
+	char *args = NULL; /* recieved argument array */
 	pid_t child_pid;
 
 	input = NULL;
 	while (!input)
 	{
 		/* checks if not an error */
+		printf("$ ");
 		if (getline(&input, &len, stdin) == -1)
 		{
 			printf("Error\n");
@@ -24,15 +72,13 @@ int main(void)
 		}
 		else
 		{
-			args = char *string_parse(input); /* recieves arr arg */
+			args = string_parse(input); /* recieves arr arg */
 			child_pid = fork(); /* makes child */
 			if (child_pid != 0)
-			{
 				wait(NULL);
-			}
 			else
 			{
-				execve(args[0], args, NULL); /* not sure if null */
+				execve((const char*)args[0], (char *const *)args, NULL); /* not sure if null */
 				return (0);
 			}
 			input = NULL;
