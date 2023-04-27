9183fef6459c30a1946b79ac4e3a177d68281b61
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-27 10:10:10 -0700

    Betty Corrections

--- exec_fun.c
+++ exec_fun.c
@@ -10,21 +10,21 @@
 
 int exec_func(char *path, char **args)
 {
-    pid_t child;
+	pid_t child;
 
-    child = fork();
-    if (child < 0)
-    {
-        perror("Error");
-        return (-1);
-    }
-    if (child > 0)
-        wait(NULL);
-    if (child == 0)
-    {
-        execve(path, args, environ);
-        return (1);
-    }
+	child = fork();
+	if (child < 0)
+	{
+		perror("Error");
+		return (-1);
+	}
+	if (child > 0)
+		wait(NULL);
+	if (child == 0)
+	{
+		execve(path, args, environ);
+		return (1);
+	}
 
-    return (1);
+	return (1);
 }
