8d17a741e29561961bb3b0a5c43312514f3b67c2
Patricio-Benglian <6305@holbertonstudents.com>
2023-04-27 10:22:11 -0700

    Fixed EOF exit code

--- shellmain.c
+++ shellmain.c
@@ -8,40 +8,40 @@
 
 int main(void)
 {
-    char **args = NULL;
-    char **path = NULL;
-    char *input = NULL;
-    char *pathenv = NULL;
-    char *filepath = NULL;
-    size_t len = 0;
+	char **args = NULL;
+	char **path = NULL;
+	char *input = NULL;
+	char *pathenv = NULL;
+	char *filepath = NULL;
+	size_t len = 0;
 
-    while (1)
-    {
-        printf("$ ");
-        if (getline(&input, &len, stdin) == -1)
-        {
-            free(input);
-            exit(1);
-        }
-        args = string_parse(input);
-        // if (is_exit(args))
-        /*{
-            free(args);
-            free(input);
-            exit(1);
-        }
-        */
-        pathenv = strdup(_getenv());
-        path = path_parse(pathenv);
+	while (1)
+	{
+		printf("$ ");
+		if (getline(&input, &len, stdin) == -1)
+		{
+			free(input);
+			exit(0);
+		}
+		args = string_parse(input);
+		// if (is_exit(args))
+		/*{
+			free(args);
+			free(input);
+			exit(1);
+		}
+		*/
+		pathenv = strdup(_getenv());
+		path = path_parse(pathenv);
 
-        filepath = _which(args[0], path);
+		filepath = _which(args[0], path);
 
-        exec_func(filepath, args);
+		exec_func(filepath, args);
 
-        free(filepath);
-        free(args);
-        free(path);
-        free(pathenv);
-    }
-    return (-1);
+		free(filepath);
+		free(args);
+		free(path);
+		free(pathenv);
+	}
+	return (-1);
 }
